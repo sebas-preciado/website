a5f88dc29239a5e0882597e6cdfd788a60c3aadf
Sebastian Preciado <111720089+sebas-preciado@users.noreply.github.com>
2023-03-21 17:08:48 +0000

    Tue, Mar 21, 2023, 5:08 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -4,6 +4,14 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = taret / 200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)} `
+        }
+
     }
 })
