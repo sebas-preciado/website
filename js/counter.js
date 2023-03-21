f05a44b578e6e36a8834645f3b7b28d1765279b3
Sebastian Preciado <111720089+sebas-preciado@users.noreply.github.com>
2023-03-21 17:04:08 +0000

    Tue, Mar 21, 2023, 5:04 PM +00:00

---
+++ js/counter.js
@@ -0,0 +1,9 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+    }
+})
