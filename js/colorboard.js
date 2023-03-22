d20ab0ecb3a5725bce67c5e3e06c4bb3af1ff196
Sebastian Preciado <111720089+sebas-preciado@users.noreply.github.com>
2023-03-22 16:29:44 +0000

    Wed, Mar 22, 2023, 4:29 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,2 +1,7 @@
 const container = document.GetElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+    
+}
