a2c2e11dbc1df1c58d542b22fcda7ad7e814bf7c
Sebastian Preciado <111720089+sebas-preciado@users.noreply.github.com>
2023-03-24 16:42:20 +0000

    Fri, Mar 24, 2023, 4:42 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -1,1 +1,11 @@
 const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300 / speedEl.val
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0,idx)
+}
